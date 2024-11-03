d05e7f0443d48633efc7f178398e8d504dcfa777
Konstantin <turbo.foxy01@gmail.com>
2024-11-03 04:11:23 +0100

    login functionality added with the ability to search for schedules

--- src/magister_session.py
+++ src/magister_session.py
@@ -1,181 +1,89 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from seleniumwire import webdriver as wire_webdriver
-import time
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 import requests
-import logging
-class MagistersSession():
-    def __init__(self, timeout_per_request = 15):
-        self.login_cookies = None
-        self.auth_token = None
+from urllib.parse import urlparse, parse_qs
+from request_manager import LoginRequestsSender
+
+
+
+
+class MagisterSession():
+    def __init__(self):
+        self.request_sender = LoginRequestsSender()
+        self.session = requests.Session()
+        self.profile_auth_token = None #auth token for the redirect page
+        self.app_auth_token = None #auth token for the main app
+        self.authcode = "72f0e355703a44e5a9"
+        self.sessionid = None
+        self.returnurl = None
+        self.main_payload = None
         self.person_id = None
-        self.school_name = None
-        self.school_domain = None
-        self.session_id = None
-        self.enable_log = True
-        self.timeout = timeout_per_request
-        
-    def authorize(self, school_name,school_domain,username,password):
-        uri = "https://accounts.magister.net/profile/"
-        self.school_name = school_name
-        self.school_domain = school_domain
-        # Configure Chrome options
-        
-        logging.getLogger('seleniumwire').setLevel(logging.WARNING)
-        options = Options()
-        options.add_argument("--log-level=3")
-        options.add_argument("--headless")  # Run in headless mode
-        options.add_argument("--blink-settings=imagesEnabled=false")  # Disable images
+        self.account_id = None
+        self.api_url = None
 
-        # Create a Selenium Wire WebDriver
-        #driver = wire_webdriver.Chrome()
-        driver = driver = wire_webdriver.Chrome(options=options)
-        wait = WebDriverWait(driver, self.timeout)
-        driver.get(uri)
-        
-        school_input = wait.until(EC.presence_of_element_located((By.ID, "scholenkiezer_value")))
-        
-        school_input.send_keys(school_name)
-        
-        time.sleep(1)
-        school_input.send_keys(Keys.RETURN)
 
-            # Step 2: Input the username and wait for the username field to appear
-        
-        username_input = wait.until(EC.presence_of_element_located((By.ID, "username")))
-        username_input.send_keys(username)
-        time.sleep(1)
-        username_input.send_keys(Keys.RETURN)
-
-            # Step 3: Input the password and wait for the password field to appear
-
-        
-        password_input = wait.until(EC.presence_of_element_located((By.ID, "password")))
-        password_input.send_keys(password)
-        password_input.send_keys(Keys.RETURN)
-        time.sleep(1)
-        wait.until(EC.url_contains("https://accounts.magister.net/profile/#/"))
-        
-        driver.get(f"https://{school_domain}.magister.net/magister/#/vandaag")
-        # Navigate to the specified URL
-        
-        
-        # Allow some time for the requests to complete
-        wait.until(EC.url_contains(f"https://{school_domain}.magister.net/magister/#/vandaag"))
-        
-
-        # Intercept requests and check for the Bearer token
-        for request in driver.requests[::-1]:
-            
-            if request.response and 'Authorization' in request.headers:
-                value = request.headers['Authorization']
-                if value.startswith('Bearer'):
-                    
-                    self.auth_token = value
-                    
-                    self.log(f"\nauth token found!")
-                    break
-                    
-                    
-        self.login_cookies = driver.get_cookies()
-        
-
-        # Clean up
-        if self.set_session_id():
-            self.log("session id set!")
-        if self.set_person_id():
-            self.log(f"person id set!{self.person_id}")
-        driver.quit()
-
-        self.log("Authorization successful!")
-    #def get_json_for_date
-    def set_session_id(self):
-        '''
-        returns true if the id session was set returns False if not
-        '''
-        if self.login_cookies:
-            for cookie in self.login_cookies:
-                if cookie["name"] == "idsrv.session":
-                    self.session_id = cookie["value"]
-                    return True
-        
-        return False
-    def get_school_rooster(self,_from,to):
-        if not(self.person_id and self.auth_token):
-            self.log("Session not authorized. Please authorize first before using the functions")
-            return None
-        url = f"https://o2groningen.magister.net/api/personen/{self.person_id}/afspraken"
-        params = {
-            "status": "1",
-            "tot": _from, #2024-11-08
-            "van": to#"2024-11-01"
-        }
-        headers = {
-            "accept": "application/json, text/plain, */*",
-            "accept-language": "en-US;q=0.8,en;q=0.7,nl;q=0.6",
-            "authorization": f"{self.auth_token}",
-            #"cookie": f"SESSION_ID={self.session_id}",
-            "priority": "u=1, i",
-            "referer": "https://o2groningen.magister.net/magister/",
-            "sec-ch-ua": '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
-            "sec-ch-ua-mobile": "?1",
-            "sec-ch-ua-platform": '"Android"',
-            "sec-fetch-dest": "empty",
-            "sec-fetch-mode": "cors",
-            "sec-fetch-site": "same-origin",
-            "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Mobile Safari/537.36"
-        }
-    
-        response = requests.get(url, headers=headers, params=params)
-        if response.status_code == 200:
-            self.log("Request was successful!")
-            return response.json()  # If JSON response is expected
-        else:
-            self.log(f"Request failed with status code {response.status_code}")
-            return response.json()["Items"]
-    def set_person_id(self):
-        url = f"https://{self.school_domain}.magister.net:443/api/toestemmingen"
-        headers = {
-            "accept": "application/json, text/plain, */*",
-            "accept-language": "en-US;q=0.8,en;q=0.7,nl;q=0.6",
-            "authorization": f"{self.auth_token}",
-            "cookie": f"SESSION_ID={self.session_id}",
-            "priority": "u=1, i",
-            "referer": "https://o2groningen.magister.net/magister/",
-            "sec-ch-ua": '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
-            "sec-ch-ua-mobile": "?1",
-            "sec-ch-ua-platform": '"Android"',
-            "sec-fetch-dest": "empty",
-            "sec-fetch-mode": "cors",
-            "sec-fetch-site": "same-origin",
-            "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Mobile Safari/537.36"
-        }
-        request = requests.get(url, headers=headers)
-        if request.status_code == 200:
-            
-            self.person_id = request.json()["items"][0]["persoonId"]
-            return True
-        
-        
-        return False
-        
-
-    def log(self,msg):
-        if self.enable_log:
+        self.recieve_log = True
+    def _logMessage(self,msg:str):
+        if self.recieve_log:
             print(msg)
 
-    
+    def login(self,school_name,username,password):
+        
+        url_login_page = "https://accounts.magister.net/"
+
+
+
+        response = self.session.get(url_login_page,allow_redirects=False)
 
 
 
 
+        redirect_url_1 = r"https://accounts.magister.net/connect/authorize?client_id=iam-profile&redirect_uri=https%3A%2F%2Faccounts.magister.net%2Fprofile%2Foidc%2Fredirect_callback.html&response_type=id_token%20token&scope=openid%20profile%20email%20magister.iam.profile&state=57dcb9c3b667407791ff32a7af41e703&nonce=ec78d557c0e44751bf573db6719445cd"
+        response = self.session.get(redirect_url_1,allow_redirects=False)
+
+        response = self.session.get(response.headers.get("location"), allow_redirects= False)
 
 
 
 
+        response = self.session.get("https://accounts.magister.net/" + response.headers.get("location"), allow_redirects= False)
+
+        self.sessionid = parse_qs(urlparse(response.url).query).get('sessionId', [None])[0]
+        self.returnurl = parse_qs(urlparse(response.url).query).get('returnUrl', [None])[0]
+        self.authcode = "72f0e355703a44e5a9"
+
+        self.main_payload = {
+        'authCode': self.authcode,
+        'returnUrl': self.returnurl, 
+        'sessionId': self.sessionid
+        }
+        self.request_sender.set_school(request_session=self.session,school_name=school_name,main_payload=self.main_payload)
+        self.request_sender.set_username(request_session=self.session,username=username,main_payload=self.main_payload)
+        self.request_sender.set_password(request_session=self.session,password=password,main_payload=self.main_payload)
+
+
+        self.profile_auth_token = self.request_sender.get_profile_auth_token(request_session=self.session)
+        self.api_url = self.request_sender.get_api_url(request_session=self.session,profile_auth_token=self.profile_auth_token)
+        self.app_auth_token = self.request_sender.get_app_auth_token(request_session=self.session,api_url=self.api_url)
+
+
+        self.account_id = self.request_sender.get_accountid(request_session=self.session,app_auth_token=self.app_auth_token,api_url=self.api_url)
+        self.person_id = self.request_sender.get_personid(request_session=self.session,app_auth_token=self.app_auth_token,api_url=self.api_url,account_id=self.account_id)
+        self._logMessage("you have successfully logged in!")
+
+    def get_schedule(self, _from:str, to:str):
+
+        if not self.app_auth_token:
+            self._logMessage("You have not logged in yet")
+            return
+
+        params = {
+        "status" : 1,
+        "tot": to,
+        "van": _from
+        }
+        headers = {"authorization":self.app_auth_token}
+        url = f"{self.api_url}/personen/{self.person_id}/afspraken"
+        respone = self.session.get(url=url,params=params,headers=headers)
+        print(url)
+        print(respone.text)
+        if respone.status_code == 200:
+            return respone.json()
