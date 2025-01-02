349d20a3f42881e424c32eeb6882cce00b899a8a
H3LL_0U <turbo.foxy01@gmail.com>
2025-01-03 00:11:34 +0100

    fix dependencies installation

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from setuptools import setup, find_packages
 
 
+def parse_requirements(filename):
+    with open(filename, 'r') as f:
+        return [line.strip() for line in f if line.strip() and not line.startswith('#')]
+
 setup(
     name="MagisterPy",  
     version="0.1.0",  
@@ -8,7 +12,6 @@ setup(
     long_description=open("README.MD").read(),  
     long_description_content_type="text/markdown", 
     author="H3LL0U",
-    
     url="https://github.com/H3LL0U/MagisterPy",  
     packages=find_packages(),  
     classifiers=[
@@ -17,6 +20,7 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.9",
-    
     include_package_data=True,
-    zip_safe=False)
+    zip_safe=False,
+    install_requires=parse_requirements('requirements.txt')  # Read dependencies
+)
